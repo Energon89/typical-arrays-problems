9e483f80b7941a20c91a181d71a5c60adf09d692
Energon89 <podymov89@gmail.com>
2020-02-26 17:30:36 +0300

    feat: add logic to functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    return Array.isArray(array) && array.length ?
+        array.reduce(
+            (minNumber, currentNumber) => {
+                if (minNumber < currentNumber) {
+                    return minNumber;
+                } else return currentNumber;
+            }
+        ) : 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    return Array.isArray(array) && array.length ?
+        array.reduce(
+            (maxNumber, currentNumber) => {
+                if (maxNumber > currentNumber) {
+                    return maxNumber;
+                } else return currentNumber;
+            }
+        ) : 0;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    return Array.isArray(array) && array.length ?
+        array.reduce(
+            (sumNumber, currentNumber) => sumNumber + currentNumber
+        ) / array.length : 0;
 }
